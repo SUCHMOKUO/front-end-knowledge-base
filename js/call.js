9a20ad8f1e22a61ab424a6d748ce62dac111a796
SUCHMOKUO <suchmokuo@gmail.com>
2020-05-26 22:39:08 +0800

    remove useless thing & add more description to call.js

--- js/call.js
+++ js/call.js
@@ -1,5 +1,8 @@
 /**
  * 非严格模式实现
+ * 无法实现：
+ *   1. 严格模式下 this 不做额外处理，可以是非对象
+ *   2. ctx 被 freeze 了，无法使用添加属性的办法使得函数获得 this
  */
 Function.prototype.call2 = function(ctx, ...args) {
   if (typeof this !== "function") {
